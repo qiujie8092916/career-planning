0eae82028093e0543d16d08e02d0b5d3fa4caecd
Jie.Q <qiujieee_empty@outlook.com>
2023-05-31 01:08:19 +0800

    :sparkles: Particles

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -10,10 +10,10 @@ import { useCreateReducer } from '@/hooks/useCreateReducer';
 
 import useErrorService from '@/services/errorService';
 import useApiService from '@/services/useApiService';
-// import Particles from "react-particles";
-// import type { Engine } from "tsparticles-engine";
-// import { loadFull } from "tsparticles";
-// import { ISourceOptions } from "tsparticles-engine";
+import Particles from "react-particles";
+import type { Engine } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
+import { ISourceOptions } from "tsparticles-engine";
 import {
   cleanConversationHistory,
   cleanSelectedConversation,
@@ -238,71 +238,71 @@ const Home = ({
     serverSideApiKeyIsSet,
   ]);
 
-  // const particlesInit = useCallback(async (engine: Engine) => {
-  //   await loadFull(engine);
-  // }, []);
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
 
-  // const particlesOptions = useMemo(() => ({
-  //   particles: {
-  //     number: { value: 100, density: { enable: true, value_area: 800 } },
-  //     color: { value: "#333" },
-  //     shape: {
-  //       type: "circle",
-  //       stroke: { width: 0, color: "#000000" },
-  //       polygon: { nb_sides: 5 },
-  //       image: { src: "img/github.svg", width: 100, height: 100 },
-  //     },
-  //     opacity: {
-  //       value: 0.5,
-  //       random: false,
-  //       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
-  //     },
-  //     size: {
-  //       value: 3,
-  //       random: true,
-  //       anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
-  //     },
-  //     line_linked: {
-  //       enable: true,
-  //       distance: 150,
-  //       color: "#aaa",
-  //       opacity: 0.4,
-  //       width: 1,
-  //     },
-  //     move: {
-  //       enable: true,
-  //       speed: 2,
-  //       direction: "none",
-  //       random: false,
-  //       straight: false,
-  //       out_mode: "out",
-  //       bounce: false,
-  //       attract: { enable: false, rotateX: 600, rotateY: 1200 },
-  //     },
-  //   },
-  //   interactivity: {
-  //     detect_on: "canvas",
-  //     events: {
-  //       onhover: { enable: true, mode: "grab" },
-  //       onclick: { enable: true, mode: "push" },
-  //       resize: true,
-  //     },
-  //     modes: {
-  //       grab: { distance: 200, line_linked: { opacity: 1 } },
-  //       bubble: {
-  //         distance: 400,
-  //         size: 40,
-  //         duration: 2,
-  //         opacity: 8,
-  //         speed: 2,
-  //       },
-  //       repulse: { distance: 200, duration: 0.4 },
-  //       push: { particles_nb: 4 },
-  //       remove: { particles_nb: 2 },
-  //     },
-  //   },
-  //   retina_detect: true,
-  // }), [])
+  const particlesOptions = useMemo(() => ({
+    particles: {
+      number: { value: 100, density: { enable: true, value_area: 800 } },
+      color: { value: "#333" },
+      shape: {
+        type: "circle",
+        stroke: { width: 0, color: "#000000" },
+        polygon: { nb_sides: 5 },
+        image: { src: "img/github.svg", width: 100, height: 100 },
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#aaa",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: { enable: false, rotateX: 600, rotateY: 1200 },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: true, mode: "grab" },
+        onclick: { enable: true, mode: "push" },
+        resize: true,
+      },
+      modes: {
+        grab: { distance: 200, line_linked: { opacity: 1 } },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 2,
+        },
+        repulse: { distance: 200, duration: 0.4 },
+        push: { particles_nb: 4 },
+        remove: { particles_nb: 2 },
+      },
+    },
+    retina_detect: true,
+  }), [])
 
   return (
     <HomeContext.Provider
@@ -313,7 +313,7 @@ const Home = ({
         handleUpdateConversation,
       }}
     >
-      {/*<Particles options={particlesOptions as ISourceOptions} init={particlesInit} />*/}
+      <Particles options={particlesOptions as ISourceOptions} init={particlesInit} />
       <Head>
         <title>Chatbot UI</title>
         <meta name="description" content="ChatGPT but better." />
