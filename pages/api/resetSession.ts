56430f76f2a2fb559e2a355fe7c35a026f9cd0e0
Jie.Q <qiujieee_empty@outlook.com>
2023-06-04 23:56:19 +0800

    :construction: setcookie domain v3

--- pages/api/resetSession.ts
+++ pages/api/resetSession.ts
@@ -3,15 +3,13 @@ export const config = {
 };
 
 const handler = async (req: Request) => {
-  console.log('host', req.headers.get('host'))
   console.log('origin', req.headers.get('origin'))
-  console.log('referer', req.headers.get('referer'))
   return new Response(
     JSON.stringify({}),
     {
       status: 200,
       headers: {
-        'Set-Cookie': `mpuid=; Path=/; Domain=${req.headers.get('host')!.split(":")[0]}; Max-Age=0; HttpOnly`
+        'Set-Cookie': `mpuid=; Path=/; Domain=${new URL(req.headers.get('origin')!).host.split(":")[0]}; Max-Age=0; HttpOnly`
       },
     },
   );
