464c891f2b7790b3c2cb976ee35e17d0c04f9921
Jie.Q <qiujieee_empty@outlook.com>
2023-06-04 21:56:01 +0800

    :lipstick: 静态资源优化

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
+  assetPrefix: process.env.cdn === 'yes' ? 'https://mypath-life.oss-cn-hongkong.aliyuncs.com' : '',
   reactStrictMode: true,
-
   webpack(config, { isServer, dev }) {
     config.experiments = {
       asyncWebAssembly: true,
