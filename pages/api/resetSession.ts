9244f0d401120c3f0d0be2f51930286f0d19a293
Jie.Q <qiujieee_empty@outlook.com>
2023-06-04 23:47:09 +0800

    :sparkles: setcookie domain

--- pages/api/resetSession.ts
+++ pages/api/resetSession.ts
@@ -2,13 +2,13 @@ export const config = {
   runtime: 'edge',
 };
 
-const handler = async () => {
+const handler = async (req: Request) => {
   return new Response(
     JSON.stringify({}),
     {
       status: 200,
       headers: {
-        'Set-Cookie': 'mpuid=; Path=/; Max-Age=0; HttpOnly',
+        'Set-Cookie': `mpuid=; Path=/; Domain=${req.headers.get('host')!.split(":")[0]}; Max-Age=0; HttpOnly`
       },
     },
   );
